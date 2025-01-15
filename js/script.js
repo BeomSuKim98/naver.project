d2a35ab8872fc3a29b68498bd8e0343e4952a38d
BeomSuKim98 <qja0427@gmail.com>
2025-01-15 13:20:07 +0900

    feed 박스 html구조와 css 완료. html내에 컨텐츠를 넣을 것.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function shoppingAdImg(){
-    let imageIndex = $('productLink');
-    console.log(imageIndex);
+function shoppingAdImg() {
+  let imageIndex = $(".productLink");
+  console.log(imageIndex);
 
-    for(let i = 10; i < imageIndex.length; i++ ){
-        $(imageIndex[i]).attr('src', 'adimg_' + (i+1));
-    }
+  for (let i = 1; i < imageIndex.length; i++) {
+    $(imageIndex[i]).attr("src", "../img/shoppingAdImg/adimg_" + i + ".jpg");
+  }
 }
 
 shoppingAdImg();
